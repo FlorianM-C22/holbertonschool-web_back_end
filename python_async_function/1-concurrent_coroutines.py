36e048de99f3c58e2e2168b2ece710918ae1b0e4
FlorianM-C22 <7484@holbertonstudents.com>
2024-04-22 14:08:29 +0200

    Task 1 - Trying to add more documentation

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
-"""Module that demonstrates concurrent coroutines"""
+""""Runs multiple coroutines at the same time with async"""
+
 
 import asyncio
+import typing
+
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> float:
+async def wait_n(n: int, max_delay: int) -> typing.List[float]:
     """
-    Asynchronous coroutine that waits for
-    a random delay between 0 and max_delay
+    Perform asynchronous waiting using wait_random.
+
+    Args:
+        n (int): Number of times to call wait_random.
+        max_delay (int): Maximum delay value for each wait_random call.
+
+    Returns:
+        typing.List[float]: List of delays in ascending order.
     """
+
     delay_n = await asyncio.gather(*[wait_random(max_delay) for _ in range(n)])
 
     return sorted(delay_n)

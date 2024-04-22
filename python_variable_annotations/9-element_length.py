d1e7a4993db60d78906b0576537fc94a74659cde
FlorianM-C22 <7484@holbertonstudents.com>
2024-04-22 12:47:25 +0200

    Task 9

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -4,9 +4,13 @@ Annotate the below functions parameters
 and return values with the appropriate types
 """
 
-from typing import List, Tuple
+import typing
 
 
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
-    """Returns a list of tuples containing a string and its length."""
+def element_length(lst: typing.Iterable[typing.Sequence],
+                   ) -> typing.List[typing.Tuple[typing.Sequence, int]]:
+    """Return a list of tuples where the first element
+    of each passed tuple is the original tuple
+    and the second element is the length of the tuple.
+    """
     return [(i, len(i)) for i in lst]

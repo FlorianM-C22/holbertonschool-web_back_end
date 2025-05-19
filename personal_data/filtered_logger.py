0b95316f5716d4ac10d0c0ecf79f6ad60c00d6f9
FlorianM-C22 <7484@holbertonstudents.com>
2025-05-19 11:55:24 +0200

    Task 0

---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+filter_datum
+"""
+
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    """
+    filter_datum
+    """
+    for field in fields:
+        message = re.sub(f"{field}=[^{separator}]+",
+                         f"{field}={redaction}", message)
+    return message

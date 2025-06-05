17d58a814da805e7ac7cd34dac50c9f6d89eaf69
Florian Meignan <7484@holbertonstudents.com>
2025-06-05 16:17:09 +0200

    Task 4

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -2,7 +2,7 @@
 """ Basic authentication module
 """
 from flask import request
-from typing import List, TypeVar
+from typing import List, TypeVar, Optional
 
 User = TypeVar('User')
 
@@ -11,10 +11,24 @@ class Auth:
     """ Auth class
     """
 
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+    def require_auth(self, path: Optional[str], excluded_paths: Optional[List[str]]) -> bool:
         """ require_auth method
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None:
+            return True
+
+        for excluded_path in excluded_paths:
+            # Make comparison slash tolerant
+            if excluded_path.endswith('/'):
+                if path == excluded_path or path == excluded_path.rstrip('/'):
+                    return False
+            else:
+                if path == excluded_path or path == excluded_path + '/':
+                    return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ authorization_header method

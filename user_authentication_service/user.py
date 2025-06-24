aea1ee74068949a8017e6eb6c17e28cee16a883f
FlorianM-C22 <7484@holbertonstudents.com>
2025-06-24 15:58:09 +0200

    More doc

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """
-User class
+This module defines the User class for the users table in the database.
+The User class represents a user with email, hashed password, session ID,
+and reset token.
 """
 
 from sqlalchemy.ext.declarative import declarative_base
@@ -10,6 +12,17 @@ Base = declarative_base()
 
 
 class User(Base):
+    """
+    SQLAlchemy User model for the users table.
+
+    Attributes:
+        id (int): Primary key for the user.
+        email (str): User's email address, must be unique and not null.
+        hashed_password (str): Hashed password for authentication, not null.
+        session_id (str): Optional session identifier for the user.
+        reset_token (str): Optional token for password reset functionality.
+    """
+
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)

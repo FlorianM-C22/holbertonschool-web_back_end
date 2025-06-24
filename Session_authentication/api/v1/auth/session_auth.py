8a6bc227cc68a9982a27bb5257b7b894c30c79a0
Jlaborne <your.github@email.com>
2025-06-24 11:47:06 +0200

    Adjustements to Task 2

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -16,6 +16,8 @@ class SessionAuth(Auth):
         """
         if user_id is None:
             return
+        if not isinstance(user_id, str):
+            return None
 
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id

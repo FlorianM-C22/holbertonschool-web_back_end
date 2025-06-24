fbd4901990286b83bbad9abf549a3c4616123871
FlorianM-C22 <7484@holbertonstudents.com>
2025-06-24 15:56:56 +0200

    Correct task 0

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""
+User class
+"""
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
@@ -8,9 +11,9 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = "users"
+
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=False)
-    reset_token = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)

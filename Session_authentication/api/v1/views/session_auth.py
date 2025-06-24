fe11851ebd473d609209858206913fa55f58d93e
FlorianM-C22 <7484@holbertonstudents.com>
2025-06-24 13:52:29 +0200

    Even more task 8

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -39,7 +39,8 @@ def login():
     return response
 
 
-@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
 def delete():
     """ Deletes the user session / logout
     """

1df7f28ec837e4159ca08acb3ecd51318a4e1907
FlorianM-C22 <7484@holbertonstudents.com>
2025-06-25 15:27:23 +0200

    5. Register user

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -5,6 +5,8 @@ Auth module
 
 import bcrypt
 from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 class Auth:
@@ -14,8 +16,17 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
+    def _hash_password(self, password: str) -> str:
+        """Hash a password
+        """
+        return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
-def _hash_password(password: str) -> str:
-    """Hash a password
-    """
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    def register_user(self, email: str, password: str) -> User:
+        """Register a new user
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = self._hash_password(password)
+            return self._db.add_user(email, hashed_password)

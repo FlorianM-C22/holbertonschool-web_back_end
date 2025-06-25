5f819d59941658b2e5f7874b19290a1a408bf884
FlorianM-C22 <7484@holbertonstudents.com>
2025-06-25 15:29:01 +0200

    Moving out hash from the class

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -9,17 +9,18 @@ from user import User
 from sqlalchemy.orm.exc import NoResultFound
 
 
+def _hash_password(password: str) -> str:
+    """Hash a password
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
 
     def __init__(self):
         self._db = DB()
-
-    def _hash_password(self, password: str) -> str:
-        """Hash a password
-        """
-        return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
     def register_user(self, email: str, password: str) -> User:
         """Register a new user
@@ -28,5 +29,5 @@ class Auth:
             self._db.find_user_by(email=email)
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
-            hashed_password = self._hash_password(password)
+            hashed_password = _hash_password(password)
             return self._db.add_user(email, hashed_password)

705916ed5962cbd6b35a11aa469bd5e02552a120
FlorianM-C22 <7484@holbertonstudents.com>
2025-06-25 15:53:58 +0200

    Fix task 11

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -8,7 +8,7 @@ from auth import Auth
 
 app = Flask(__name__)
 
-auth = Auth()
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
@@ -21,7 +21,7 @@ def users():
     email = request.form.get('email')
     password = request.form.get('password')
     try:
-        user = auth.register_user(email, password)
+        user = AUTH.register_user(email, password)
         return jsonify({"email": user.email, "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
@@ -31,9 +31,9 @@ def users():
 def login():
     email = request.form.get('email')
     password = request.form.get('password')
-    if not auth.valid_login(email, password):
+    if not AUTH.valid_login(email, password):
         abort(401)
-    session_id = auth.create_session(email)
+    session_id = AUTH.create_session(email)
     response = jsonify({"email": email, "message": "logged in"})
     response.set_cookie("session_id", session_id)
     return response

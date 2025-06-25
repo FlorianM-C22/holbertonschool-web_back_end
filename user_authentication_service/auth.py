290ee24d23a5cd8d73976ff307a1f1a986515e78
FlorianM-C22 <7484@holbertonstudents.com>
2025-06-25 15:33:48 +0200

    8. Credentials validation

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -31,3 +31,12 @@ class Auth:
         except NoResultFound:
             hashed_password = _hash_password(password)
             return self._db.add_user(email, hashed_password)
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validate login
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password.encode('utf-8'))
+        except NoResultFound:
+            return False

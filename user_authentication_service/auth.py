fbc4a7ec439561ca7f5e2e6edbd70ebbf9f2220a
FlorianM-C22 <7484@holbertonstudents.com>
2025-06-25 16:04:50 +0200

    18. Update password

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -87,3 +87,13 @@ class Auth:
             return reset_token
         except NoResultFound:
             raise ValueError(f"User {email} not found")
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Update a password
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed_password = _hash_password(password)
+            self._db.update_user(user.id, hashed_password=hashed_password)
+        except NoResultFound:
+            raise ValueError(f"User {reset_token} not found")

89c62d41eb7a8ebbf5eb9b28bfb177672b4a32dd
FlorianM-C22 <7484@holbertonstudents.com>
2025-07-11 13:21:55 +0200

    Task 2 fix

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -21,12 +21,14 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-@babel.localeselector
 def get_locale():
     """
     Get the locale from the request
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route('/')

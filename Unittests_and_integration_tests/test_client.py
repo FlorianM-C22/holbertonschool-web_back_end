a8566a5e6be80e2c9eeb348622d14787a6a39e02
FlorianM-C22 <7484@holbertonstudents.com>
2025-07-11 12:03:31 +0200

    Task 4

---
+++ Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python3
+
+"""Test client module.
+"""
+
+import unittest
+from unittest.mock import patch, Mock
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Test class for GithubOrgClient class."""
+
+    @parameterized.expand([
+        ("google", {"login": "google"}),
+        ("abc", {"login": "abc"}),
+    ])
+    def test_org(self, org_name, expected_repos):
+        """Test GithubOrgClient.org method."""
+        with patch('client.get_json') as mock_get_json:
+            mock_get_json.return_value = expected_repos
+            client = GithubOrgClient(org_name)
+            self.assertEqual(client.org, expected_repos)
+            mock_get_json.assert_called_once_with(
+                f"https://api.github.com/orgs/{org_name}")
+
+    @parameterized.expand([
+        ("google", {"repos_url": "https://api.github.com/orgs/google/repos"}),
+        ("abc", {"repos_url": "https://api.github.com/orgs/abc/repos"}),
+    ])

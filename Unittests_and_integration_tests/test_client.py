2991338167bcfdfbb92f5eba4df558243a4af146
FlorianM-C22 <7484@holbertonstudents.com>
2025-07-11 12:05:16 +0200

    Task 4 fix

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
-
 """Test client module.
 """
-
 import unittest
-from unittest.mock import patch, Mock
+from unittest.mock import patch
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -16,16 +14,11 @@ class TestGithubOrgClient(unittest.TestCase):
         ("google", {"login": "google"}),
         ("abc", {"login": "abc"}),
     ])
-    def test_org(self, org_name, expected_repos):
+    @patch('client.get_json')
+    def test_org(self, org_name, expected_repos, mock_get_json):
         """Test GithubOrgClient.org method."""
-        with patch('client.get_json') as mock_get_json:
-            mock_get_json.return_value = expected_repos
-            client = GithubOrgClient(org_name)
-            self.assertEqual(client.org, expected_repos)
-            mock_get_json.assert_called_once_with(
-                f"https://api.github.com/orgs/{org_name}")
-
-    @parameterized.expand([
-        ("google", {"repos_url": "https://api.github.com/orgs/google/repos"}),
-        ("abc", {"repos_url": "https://api.github.com/orgs/abc/repos"}),
-    ])
+        mock_get_json.return_value = expected_repos
+        client = GithubOrgClient(org_name)
+        self.assertEqual(client.org, expected_repos)
+        mock_get_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{org_name}")

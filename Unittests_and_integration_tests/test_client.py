d81eede333ee65300aa57eac2bd38de3fece45d9
FlorianM-C22 <7484@holbertonstudents.com>
2025-07-11 12:56:46 +0200

    Task 6

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -32,3 +32,25 @@ class TestGithubOrgClient(unittest.TestCase):
             client = GithubOrgClient("test")
             self.assertEqual(client._public_repos_url,
                              "https://api.github.com/orgs/test/repos")
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        """Test GithubOrgClient.public_repos method."""
+        test_payload = [
+            {"name": "repo1", "license": {"key": "apache-2.0"}},
+            {"name": "repo2", "license": {"key": "mit"}},
+            {"name": "repo3", "license": {"key": "apache-2.0"}}
+        ]
+        mock_get_json.return_value = test_payload
+
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mock_url:
+            mock_url.return_value = "https://api.github.com/orgs/test/repos"
+            client = GithubOrgClient("test")
+
+            repos = client.public_repos()
+
+            self.assertEqual(repos, ["repo1", "repo2", "repo3"])
+            mock_url.assert_called_once()
+            mock_get_json.assert_called_once_with(
+                "https://api.github.com/orgs/test/repos")

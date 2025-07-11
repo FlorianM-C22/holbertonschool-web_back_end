901ba50195c0ed28811bf014cc0edfb02e505e20
FlorianM-C22 <7484@holbertonstudents.com>
2025-07-11 13:09:09 +0200

    Fix task 0

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -2,11 +2,11 @@
 """
 A simple flask app
 """
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return
+    return render_template('0-index.html')

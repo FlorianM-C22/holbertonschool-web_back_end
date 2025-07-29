05309a223e4ee9380f728024a177bd7a19bc9b36
FlorianM-C22 <7484@holbertonstudents.com>
2025-07-29 10:30:16 +0200

    Task 1

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -5,7 +5,7 @@ Redis Cache Module
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -23,3 +23,25 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes,
+                                                          int, float]:
+        """
+        Get the data from the cache
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        return fn(data) if fn else data
+
+    def get_str(self, key: str) -> str:
+        """
+        Get the data from the cache as a string
+        """
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        """
+        Get the data from the cache as an integer
+        """
+        return self.get(key, int)

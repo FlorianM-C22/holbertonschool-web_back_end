9aaf649122bb8faf9440a0eec9b5227288634a00
FlorianM-C22 <7484@holbertonstudents.com>
2025-08-01 10:26:33 +0200

    Task 6 fix

--- MySQL_Advanced/6-bonus.sql
+++ MySQL_Advanced/6-bonus.sql
@@ -2,19 +2,22 @@
 
 DELIMITER //
 CREATE PROCEDURE AddBonus (
-    IN user_id INT,
-    IN bonus_name VARCHAR(255),
-    IN bonus_amount DECIMAL(10, 2)
-)
+	IN user_id INT,
+	IN project_name VARCHAR(255),
+	IN score INT)
+BEGIN
 
 DECLARE project_id INT;
 SELECT id INTO project_id FROM projects WHERE name = project_name;
 
 IF project_id IS NULL THEN
-    INSERT INTO projects (name) VALUES (project_name);
-    SET project_id = LAST_INSERT_ID();
-END IF;
+	INSERT INTO projects (name) VALUES (project_name);
+	SET project_id = LAST_INSERT_ID();
+end IF;
 
-INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
-END //
-DELIMITER ;
+INSERT INTO corrections(user_id, project_id, score)
+VALUES (user_id, project_id, score);
+
+END;
+//
+DELIMITER;

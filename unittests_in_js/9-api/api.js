2725b23da096394a6ffe643bf6af6fef28c0f428
FlorianM-C22 <7484@holbertonstudents.com>
2025-09-05 13:17:14 +0200

    Task 9

---
+++ unittests_in_js/9-api/api.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express()
+const port = 7865
+
+
+app.get('/', (req, res) => {
+  res.status(200).send('Welcome to the payment system')
+});
+
+app.get('/cart/:id(\\d+)', (req, res) => {
+  const cart_id = req.params.id;
+  res.status(200).send(`Payment methods for cart ${cart_id}`);
+});
+
+if (require.main === module) {
+  const PORT = 7865;
+  app.listen(PORT, () => {
+    console.log(`API available on localhost port ${PORT}`);
+  });
+}
+
+
+module.exports = app;

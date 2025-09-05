47a37c9017eb53f86ccc8a92e28cbc30c32a8620
FlorianM-C22 <7484@holbertonstudents.com>
2025-09-05 13:24:43 +0200

    add express json

--- unittests_in_js/10-api/api.js
+++ unittests_in_js/10-api/api.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
-const port = 7865
 
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.status(200).send('Welcome to the payment system')

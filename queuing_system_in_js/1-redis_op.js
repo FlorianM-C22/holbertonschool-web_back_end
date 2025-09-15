0e9dbac90c7375926a3b2401a3f9515ca3aefe79
FlorianM-C22 <7484@holbertonstudents.com>
2025-09-15 08:32:19 +0200

    Task 2

---
+++ queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,49 @@
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err}`);
+});
+
+async function setNewSchool(schoolName, value, callback) {
+  try {
+    const reply = await client.set(schoolName, value);
+    console.log(`Reply: ${reply}`);
+    if (callback) callback(null, reply);
+  } catch (err) {
+    if (callback) callback(err, null);
+  }
+}
+
+async function displaySchoolValue(schoolName, callback) {
+  try {
+    const reply = await client.get(schoolName);
+    console.log(reply);
+    if (callback) callback(null, reply);
+  } catch (err) {
+    if (callback) callback(err, null);
+  }
+}
+
+async function main() {
+  await client.connect();
+  
+  displaySchoolValue('Holberton', (err, result) => {
+    if (err) console.error('Error:', err);
+  });
+  
+  setNewSchool('HolbertonSanFrancisco', '100', (err, result) => {
+    if (err) console.error('Error:', err);
+  });
+  
+  displaySchoolValue('HolbertonSanFrancisco', (err, result) => {
+    if (err) console.error('Error:', err);
+  });
+}
+
+main();

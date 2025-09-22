edc2f5e531a0a459488af662f7c69b84d1f260fc
FlorianM-C22 <7484@holbertonstudents.com>
2025-09-22 08:20:20 +0200

    Task 4

---
+++ queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,33 @@
+import { createClient } from 'redis';
+import redis from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err}`);
+});
+
+async function main() {
+  await client.connect();
+  
+  client.hset('HolbertonSchools', 'Portland', '50', redis.print);
+  client.hset('HolbertonSchools', 'Seattle', '80', redis.print);
+  client.hset('HolbertonSchools', 'New York', '20', redis.print);
+  client.hset('HolbertonSchools', 'Bogota', '20', redis.print);
+  client.hset('HolbertonSchools', 'Cali', '40', redis.print);
+  client.hset('HolbertonSchools', 'Paris', '2', redis.print);
+  
+  client.hgetall('HolbertonSchools', (err, result) => {
+    if (err) {
+      console.error('Error:', err);
+    } else {
+      console.log(result);
+    }
+  });
+}
+
+main();
